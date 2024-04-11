082933d45ddbb590c6a163a1a7d0ac42394b5e13
Gaurav P. Malpedi <gaurav.malpedi17@gmail.com>
2024-04-11 16:31:18 +0530

    config.json update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    distDir: 'build',
+    typescript: {
+        ignoreBuildErrors: true
+    }
+};
 
 export default nextConfig;
